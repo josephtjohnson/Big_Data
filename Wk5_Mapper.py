d3485e29382d1f3e40f7877f686af907a5668f5e
Joseph Johnson <82980802+josephtjohnson@users.noreply.github.com>
2023-11-07 15:49:01 -0500

    Create Wk5_Mapper.py

---
+++ Wk5_Mapper.py
@@ -0,0 +1,16 @@
+import sys
+import csv
+
+reader = csv.reader(syst.stdin, delimiter=':')
+
+power = 0.0
+
+for line in reader:
+    #try to store power as a float
+    try:
+        power = float(line[2])
+    except:
+        continue
+        
+    #store the key as the date and the value as power
+    print line[0], "\t", power
