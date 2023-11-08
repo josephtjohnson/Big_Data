101ef51e03276bb89a1d5f6ba94264713a9019fa
Joseph Johnson <82980802+josephtjohnson@users.noreply.github.com>
2023-11-07 20:29:26 -0500

    Update SMA_Mapper.py

--- SMA_Mapper.py
+++ SMA_Mapper.py
@@ -4,16 +4,15 @@ import sys
 #read each line from the input
 for line in sys.stdin:
     kvp = {}
-    try
+    try:
         #strip white spaces
         val = line.strip()
         
         #split at the comma
         vals = val.split(",")
-        
-        #return the individual values from the split - stock code, date, and price
-        yield kvp[vals[0]] = (vals[1],vals[2])
-    except
+        kvp[vals[0]] = (vals[1],vals[2])
+        print(kvp)        
+    except:
         continue
 
 #cat sample.txt | python SMA_Mapper.py | python SMA_Reducer.py
